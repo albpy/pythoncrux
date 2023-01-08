b05c1b5e5fc7fdac4d7e7e1664312843627bc5f2
albpy <albinjos64@gmail.com>
2023-01-08 08:34:32 +0000

    numpy1

---
+++ functionalpySept22/lstcomprehension.py
@@ -0,0 +1,23 @@
+"""----------------------Questions--------------------"""
+#1. 10 to 50
+
+#2.1 to 10 square
+
+#3. 25 to 50 cube
+
+#4.15 to 25 elements 5 add
+
+
+#=======================Question 1================
+lst=[i for i in range(10,51)]
+print(lst)
+#=======================Question 2================
+lstt=[i**2 for i in range(1,11)]
+print(lstt)
+
+#=======================Question 3================
+lsttt=[i**3 for i in range(25,51)]
+print(lsttt)
+#=======================Question4=================
+lsttt=[i+5 for i in range(15,25)]
+print(lsttt)
---
+++ functionalpySept22/sqreOfNoLambda.py
@@ -0,0 +1,7 @@
+sqre=lambda num:num**2
+print(sqre(50))
+
+#Checking Even
+
+oddOreven=lambda num:num%2==0
+print(oddOreven(29))
